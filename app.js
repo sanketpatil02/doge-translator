d23987a10e1c246317ae62d5d20ec883a5c83a9f
Sanket Patil <61304918+sanketpatil02@users.noreply.github.com>
2021-08-18 17:02:07 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var txtInput = document.querySelector("#text-input");
+var btnTranslate = document.querySelector("#btn-translatebanana");
+var showOutput = document.querySelector("#output");
+
+btnTranslate.addEventListener("click", clickHandler);
+
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return url + "?text=" + text;
+}
+
+function clickHandler() {
+    var textInput = txtInput.value;
+
+    fetch(getTranslationURL(textInput))
+        .then(response => response.json())
+        .then(json => {
+            var translatedOutput = json.contents.translated;
+            showOutput.innerText = translatedOutput;
+        })
+}
